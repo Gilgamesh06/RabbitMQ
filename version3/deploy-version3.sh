f30732546ebbb2b8f0094835ff5d668be4853787
cristian <crisiandavidfuentes@hotmail.com>
2025-03-28 06:35:18 +0000

    CRF-Feat: deploy version3

--- version3/deploy-version3.sh
+++ version3/deploy-version3.sh
@@ -18,17 +18,13 @@ ssh student@10.6.101.95 "
     sudo systemctl start docker && sudo systemctl enable docker;
   fi;
 
-  # Clonar el repositorio si no está presente
-  if [ -d \"$WORK_DIR\" ]; then
-      echo 'Repositorio ya clonado en $WORK_DIR. Haciendo git pull...';
-      cd $WORK_DIR && git pull;  # Hace git pull para obtener los últimos cambios
-  else
-      git clone $REPO_URL $WORK_DIR;
-  fi;
+  # Clonar el repositorio
+  rm -r -f $WORK_DIR;
+  git clone $REPO_URL $WORK_DIR;
 
-  # Ejecutar Docker Compose en la máquina local (version 2)
+  # Ejecutar Docker Compose en la máquina local (version 3)
   docker stop \$(docker ps -q);
-  cd $WORK_DIR/version2 && docker-compose -f docker-compose-cola.yml up -d;
+  cd $WORK_DIR/version3 && docker-compose -f docker-compose-cola.yml up -d;
 "
 
 echo "Despliegue completado para Script en la máquina local (10.6.101.95)."
@@ -42,18 +38,14 @@ ssh student@10.6.101.96 "
     sudo systemctl start docker && sudo systemctl enable docker;
   fi;
 
-  # Instalar Java si no está instalado
+  # Instalar Java
   echo 'Java no está instalado. Instalando OpenJDK 17...';
   sudo apt-get install -y openjdk-17-jdk;
   java -version;
 
-  # Clonar el repositorio si no está presente
-  if [ -d \"$WORK_DIR\" ]; then
-      echo 'Repositorio ya clonado en $WORK_DIR. Haciendo git pull...';
-      cd $WORK_DIR && git pull;  # Hace git pull para obtener los últimos cambios
-  else
-      git clone $REPO_URL $WORK_DIR;
-  fi;
+  # Clonar el repositorio
+  rm -r -f $WORK_DIR;
+  git clone $REPO_URL $WORK_DIR;
 
   # Dar permisos de ejecución al archivo 'wait-for-it.sh'
   chmod 777 $WORK_DIR/agents/Consumidor-FastAPI/wait-for-it.sh;
@@ -62,9 +54,40 @@ ssh student@10.6.101.96 "
   cd $WORK_DIR/agents/Consumidor-SpringBoot/bibliotecaMalaga && ./mvnw clean install;
   cd $WORK_DIR/agents/Productor-SpringBoot/agregarLibro && ./mvnw clean install;
 
-  # Ejecutar Docker Compose en la máquina local (version 2)
+  # Ejecutar Docker Compose en la máquina local (version 3)
   docker stop \$(docker ps -q);
-  cd $WORK_DIR/version2 && docker-compose -f docker-compose-agents.yml up -d;
+  cd $WORK_DIR/version3 && docker-compose -f docker-compose-consumers.yml up -d;
 "
 
 echo "Despliegue completado para Script en la máquina local (10.6.101.96)."
+
+ssh student@10.6.101.125 "
+  # Instalar Docker si no está instalado
+  if ! command -v docker &> /dev/null; then
+    echo 'Docker no está instalado. Instalando Docker...';
+    sudo apt-get update && sudo apt-get install -y docker.io;
+    sudo systemctl start docker && sudo systemctl enable docker;
+  fi;
+
+  # Instalar Java
+  echo 'Java no está instalado. Instalando OpenJDK 17...';
+  sudo apt-get install -y openjdk-17-jdk;
+  java -version;
+
+  # Clonar el repositorio
+  rm -r -f $WORK_DIR;
+  git clone $REPO_URL $WORK_DIR;
+
+  # Dar permisos de ejecución al archivo 'wait-for-it.sh'
+  chmod 777 $WORK_DIR/agents/Consumidor-FastAPI/wait-for-it.sh;
+
+  # Ejecutar Maven Wrapper (mvnw) si es necesario
+  cd $WORK_DIR/agents/Consumidor-SpringBoot/bibliotecaMalaga && ./mvnw clean install;
+  cd $WORK_DIR/agents/Productor-SpringBoot/agregarLibro && ./mvnw clean install;
+
+  # Ejecutar Docker Compose en la máquina local (version 3)
+  docker stop \$(docker ps -q);
+  cd $WORK_DIR/version3 && docker-compose -f docker-compose-productors.yml up -d;
+"
+
+echo "Despliegue completado para Script en la máquina local (10.6.101.125)."
